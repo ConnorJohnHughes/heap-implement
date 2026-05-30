a590e8618192eda5526658277e7915d6b67e9bd4
SouthBennett <146032836+SouthBennett@users.noreply.github.com>
2026-05-29 18:11:43 -0700

    added another addHeap Junit test

--- src/HeapTest.java
+++ src/HeapTest.java
@@ -3,12 +3,22 @@ import org.junit.Test;
 
 public class HeapTest {
 
-@Test
-public void testAddOneValue() {
-  Heap heap = new Heap();
+  @Test
+  public void testAddOneValue() {
+    Heap heap = new Heap();
 
-  heap.addHeap(10);
+    heap.addHeap(10);
 
-  assertEquals("[10]", heap.getHeap().toString());
-}
+    assertEquals("[10]", heap.getHeap().toString());
+  }
+
+  @Test
+  public void testBubbleUpToRoot() {
+    Heap heap = new Heap();
+    
+    heap.addHeap(10);
+    heap.addHeap(5);
+
+    assertEquals("[5, 10]", heap.getHeap().toString());
+  }
 }
